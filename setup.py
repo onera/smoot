dcdd1457afb3c3fc51df83275b4ddd1ddb1a07ea
relf <remi.lafage@onera.fr>
2021-04-21 13:07:53 +0200

    Add initial setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smoot",
+    version="0.0.1",
+    author="Nathalie Bartoli, Rémi Lafage",
+    description="Surrogate based multi-objective optimization tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD-3",
+    url="https://github.com/OneraHub/smoot",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "smoot"},
+    packages=setuptools.find_packages(where="smoot"),
+    python_requires=">=3.6",
+    install_requires=["smt", "pymoo"],
+)
