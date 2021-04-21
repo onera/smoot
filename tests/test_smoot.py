d59ba6c18fc568230ca9621a7a448d3bb352d3ae
relf <remi.lafage@onera.fr>
2021-04-21 13:21:59 +0200

    Init empty test

---
+++ tests/test_smoot.py
@@ -0,0 +1,18 @@
+"""
+Author: Dr. John T. Hwang <hwangjt@umich.edu>
+
+This package is distributed under New BSD license.
+"""
+import numpy as np
+import unittest
+
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_smoot(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
