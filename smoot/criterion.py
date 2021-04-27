d1429fcdb2d22ac289d72c5607e3ff6eadcf0c9b
RobinGRAPIN <robingrapin@orange.fr>
2021-04-27 12:45:28 +0200

    black

--- smoot/criterion.py
+++ smoot/criterion.py
@@ -7,8 +7,8 @@ Created on Mon Apr 26 10:26:43 2021
 import numpy as np
 from scipy.stats import norm
 
+
 class criterion(object):
-    
     def __init__(self, name, models, ref=None, s=None):
         self.modeles = models
         self.name = name
@@ -19,12 +19,12 @@ class criterion(object):
         if self.name == "PI":
             return self.PI(x)
         if self.name == "EHVI":
-            return self.EHVI(x,self.ref)
+            return self.EHVI(x, self.ref)
         if self.name == "WB2S":
             return self.WB2S(x, self.ref, self.s)
-        
+
     # Caution !!!! 2-d objective space only for the moment !!!
-    def PI(self,x ):
+    def PI(self, x):
         """
         Parameters
         ----------
@@ -37,35 +37,42 @@ class criterion(object):
             PI(x) : probability that x is an improvement € [0,1]
         """
 
-        ydata = np.transpose(np.asarray([mod.training_points[None][0][1] for mod in self.modeles]))[0]
+        ydata = np.transpose(
+            np.asarray([mod.training_points[None][0][1] for mod in self.modeles])
+        )[0]
         pareto_index = self.pareto(ydata)
         pareto_front = [ydata[i] for i in pareto_index]
         moyennes = [mod.predict_values for mod in self.modeles]
-        variances = [mod.predict_variances for mod in self.modeles]#racine d'une fction ne marche pas,je fais donc en 2 temps pour ecart-type
+        variances = [
+            mod.predict_variances for mod in self.modeles
+        ]  # racine d'une fction ne marche pas,je fais donc en 2 temps pour ecart-type
         x = np.asarray(x).reshape(1, -1)
-        sig1, sig2 = variances[0](x)[0][0]**0.5, variances[1](x)[0][0]**0.5
+        sig1, sig2 = variances[0](x)[0][0] ** 0.5, variances[1](x)[0][0] ** 0.5
         moy1, moy2 = moyennes[0](x)[0][0], moyennes[1](x)[0][0]
         m = len(pareto_front)
-        try :
-            pi_x = norm.cdf((pareto_front[0][0] - moy1)/sig1)
-            for i in range(1,m-1):
-                pi_x += ((norm.cdf((pareto_front[i+1][0] - moy1)/sig1)
-                         - norm.cdf((pareto_front[i][0] - moy1)/sig1))
-                         * norm.cdf((pareto_front[i+1][1] - moy2)/sig2))
-            pi_x += (1 - norm.cdf((pareto_front[m-1][0] - moy1)/sig1))*norm.cdf((pareto_front[m-1][1] - moy2)/sig2)
+        try:
+            pi_x = norm.cdf((pareto_front[0][0] - moy1) / sig1)
+            for i in range(1, m - 1):
+                pi_x += (
+                    norm.cdf((pareto_front[i + 1][0] - moy1) / sig1)
+                    - norm.cdf((pareto_front[i][0] - moy1) / sig1)
+                ) * norm.cdf((pareto_front[i + 1][1] - moy2) / sig2)
+            pi_x += (1 - norm.cdf((pareto_front[m - 1][0] - moy1) / sig1)) * norm.cdf(
+                (pareto_front[m - 1][1] - moy2) / sig2
+            )
             return pi_x
-        except : #for training points -> having variances = 0
+        except:  # for training points -> having variances = 0
             print("training x called : ", x)
             return 0
-    
-    def psi(self,a,b,µ,s):
-        return s*norm.pdf((b-µ)/s) + (a-µ)*norm.cdf((b-µ)/s)   
-      
+
+    def psi(self, a, b, µ, s):
+        return s * norm.pdf((b - µ) / s) + (a - µ) * norm.cdf((b - µ) / s)
+
     # Caution !!!! 2-d objective space only for the moment !!!
-    def EHVI(self,x, ref):
+    def EHVI(self, x, ref):
         """
         Expected hypervolume improvement if x is the new point added
-        
+
         Parameters
         ----------
         x : list
@@ -82,26 +89,35 @@ class criterion(object):
         """
         x = np.asarray(x).reshape(1, -1)
         variances = [mod.predict_variances for mod in self.modeles]
-        s1, s2 = variances[0](x)[0][0]**0.5, variances[1](x)[0][0]**0.5
-        if s1 == 0 or s2 == 0: #training point
+        s1, s2 = variances[0](x)[0][0] ** 0.5, variances[1](x)[0][0] ** 0.5
+        if s1 == 0 or s2 == 0:  # training point
             return 0
-        ydata = np.transpose(np.asarray([mod.training_points[None][0][1] for mod in self.modeles]))[0]
+        ydata = np.transpose(
+            np.asarray([mod.training_points[None][0][1] for mod in self.modeles])
+        )[0]
         pareto_index = self.pareto(ydata)
-        f = [ydata[i] for i in pareto_index]# pareto front
+        f = [ydata[i] for i in pareto_index]  # pareto front
         moyennes = [mod.predict_values for mod in self.modeles]
         µ1, µ2 = moyennes[0](x)[0][0], moyennes[1](x)[0][0]
         f.sort(key=lambda x: x[0])
-        f.insert(0, np.array([ref[0],-1e15]))#1e15 to approximate infinity
-        f.append( np.array([-1e15, ref[1]]))
+        f.insert(0, np.array([ref[0], -1e15]))  # 1e15 to approximate infinity
+        f.append(np.array([-1e15, ref[1]]))
         res1, res2 = 0, 0
-        for i in range(len(f)-1):
-            res1 += (f[i][0]-f[i+1][0])*norm.cdf((f[i+1][0] - µ1)/s1)*self.psi(f[i+1][1],f[i+1][1],µ2,s2)
-            res2 += (self.psi(f[i][0],f[i][0],µ1,s1) - self.psi(f[i][0],f[i+1][0],µ1,s1) )*self.psi(f[i][1],f[i][1],µ2,s2)
+        for i in range(len(f) - 1):
+            res1 += (
+                (f[i][0] - f[i + 1][0])
+                * norm.cdf((f[i + 1][0] - µ1) / s1)
+                * self.psi(f[i + 1][1], f[i + 1][1], µ2, s2)
+            )
+            res2 += (
+                self.psi(f[i][0], f[i][0], µ1, s1)
+                - self.psi(f[i][0], f[i + 1][0], µ1, s1)
+            ) * self.psi(f[i][1], f[i][1], µ2, s2)
         return res1 + res2
-        
-    def WB2S(self,x, ref, s):
+
+    def WB2S(self, x, ref, s):
         """
-        Criterion WB2S multi-objective adapted from the paper "Adapated 
+        Criterion WB2S multi-objective adapted from the paper "Adapated
         modeling strategy for constrained optimization with application
         to aerodynamic wing design" :
         WB2S(x) = s*EHVI(x) - sum( µi(x) )
@@ -123,9 +139,9 @@ class criterion(object):
         moyennes = [mod.predict_values for mod in self.modeles]
         µ = [moy(x)[0][0] for moy in moyennes]
         y = sum(µ)
-        return s*self.EHVI(x, ref) - y
-    
-    def pareto(self,Y):
+        return s * self.EHVI(x, ref) - y
+
+    def pareto(self, Y):
         """
         Parameters
         ----------
@@ -137,26 +153,28 @@ class criterion(object):
         index : list
             list of the indexes in Y of the Pareto-optimal points.
         """
-        index = [] #indexes of the best points (Pareto)
+        index = []  # indexes of the best points (Pareto)
         n = len(Y)
-        dominated = [False]*n
+        dominated = [False] * n
         for y in range(n):
             if not dominated[y]:
-                for y2 in range(y+1,n):
-                    if not dominated[y2]:#if y2 is dominated (by y0), we already compared y0 to y
-                        y_domine_y2 , y2_domine_y = self.dominate_min(Y[y],Y[y2])
-                        
-                        if y_domine_y2 :
-                            dominated[y2]=True
-                        if y2_domine_y :
-                            dominated[y]=True
+                for y2 in range(y + 1, n):
+                    if not dominated[
+                        y2
+                    ]:  # if y2 is dominated (by y0), we already compared y0 to y
+                        y_domine_y2, y2_domine_y = self.dominate_min(Y[y], Y[y2])
+
+                        if y_domine_y2:
+                            dominated[y2] = True
+                        if y2_domine_y:
+                            dominated[y] = True
                             break
                 if not dominated[y]:
                     index.append(y)
         return index
-                    
-    # returns a-dominates-b , b-dominates-a !! for minimization !!  
-    def dominate_min(self,a,b):
+
+    # returns a-dominates-b , b-dominates-a !! for minimization !!
+    def dominate_min(self, a, b):
         """
         Parameters
         ----------
@@ -177,14 +195,14 @@ class criterion(object):
         for i in range(len(a)):
             if a[i] < b[i]:
                 a_bat_b = True
-                if b_bat_a :
-                    return False, False # same front
+                if b_bat_a:
+                    return False, False  # same front
             if a[i] > b[i]:
                 b_bat_a = True
-                if a_bat_b :
-                    return False, False    
+                if a_bat_b:
+                    return False, False
         if a_bat_b and (not b_bat_a):
             return True, False
         if b_bat_a and (not a_bat_b):
             return False, True
-        return False, False # same values
+        return False, False  # same values
--- smoot/smoot.py
+++ smoot/smoot.py
@@ -25,99 +25,139 @@ from smoot.criterion import criterion
 
 #%% Optimization loop incrementing the surrogates
 
+
 class MOO(SurrogateBasedApplication):
-    
     def _initialize(self):
-        
+
         super()._initialize()
         declare = self.options.declare
 
         declare("fun", None, types=FunctionType, desc="Function to minimize")
-        declare("criterion","PI",types=str,values=["PI","EHVI", "GA","WB2S"],
+        declare(
+            "criterion",
+            "PI",
+            types=str,
+            values=["PI", "EHVI", "GA", "WB2S"],
             desc="criterion for next evaluation point determination: Expected Improvement, \
-            Surrogate-Based Optimization or genetic algo point",)
+            Surrogate-Based Optimization or genetic algo point",
+        )
         declare("n_iter", 10, types=int, desc="Number of optimizer steps")
-        declare("n_max_optim",20,types=int,
-                desc="Maximum number of internal optimizations",)
+        declare(
+            "n_max_optim",
+            20,
+            types=int,
+            desc="Maximum number of internal optimizations",
+        )
         declare("xlimits", None, types=np.ndarray, desc="Bounds of function fun inputs")
         declare("n_start", 20, types=int, desc="Number of optimization start points")
-        declare("n_parallel",1,types=int,
-            desc="Number of parallel samples to compute using qEI criterion",)
-        declare("surrogate",KRG(print_global=False),types=(KRG, KPLS, KPLSK, MGP),
+        declare(
+            "n_parallel",
+            1,
+            types=int,
+            desc="Number of parallel samples to compute using qEI criterion",
+        )
+        declare(
+            "surrogate",
+            KRG(print_global=False),
+            types=(KRG, KPLS, KPLSK, MGP),
             desc="SMT kriging-based surrogate model used internaly",
-        )#ne pas utiliser ou adapter au multiobj qu'on aie bien des modeles indep pour chaque objectif
-        declare("pop_size",100,types=int,
-            desc="number of individuals for the genetic algorithm",)
-        declare("n_gen",100,types=int,
-            desc="number generations for the genetic algorithm",)
-        declare("q",0.5,types=float,
-            desc="importance ratio of design space in comparation to objective space when chosing a point with GA",)
+        )  # ne pas utiliser ou adapter au multiobj qu'on aie bien des modeles indep pour chaque objectif
+        declare(
+            "pop_size",
+            100,
+            types=int,
+            desc="number of individuals for the genetic algorithm",
+        )
+        declare(
+            "n_gen",
+            100,
+            types=int,
+            desc="number generations for the genetic algorithm",
+        )
+        declare(
+            "q",
+            0.5,
+            types=float,
+            desc="importance ratio of design space in comparation to objective space when chosing a point with GA",
+        )
         declare("verbose", False, types=bool, desc="Print computation information")
         declare("xdoe", None, types=np.ndarray, desc="Initial doe inputs")
         declare("ydoe", None, types=np.ndarray, desc="Initial doe outputs")
-        
+
     def optimize(self, fun):
         """
         Optimize the multi-objective function fun. At the end, the object's item
         .modeles is a SMT surrogate_model object with the most precise fun's model
         .result is the result of its optimization thanks to NSGA2
-        
+
         Parameters
         ----------
         fun : function
-            function taking x=ndarray[ne,ndim], 
+            function taking x=ndarray[ne,ndim],
             returning y = [ndarray[ne, 1],ndarray[ne, 1],...]
             where y[i][j][0] = fi(xj).
         """
-        if type(self.options["xlimits"]) != np.ndarray :
-            try :
-                self.options["xlimits"]=fun.xlimits
-            except : #if fun doesn't have "xlimits" attribute
+        if type(self.options["xlimits"]) != np.ndarray:
+            try:
+                self.options["xlimits"] = fun.xlimits
+            except:  # if fun doesn't have "xlimits" attribute
                 print("Error : No bounds given")
                 return
         x_data, y_data = self._setup_optimizer(fun)
         self.ndim = self.options["xlimits"].shape[0]
-        #n_parallel = self.options["n_parallel"]
-        try :
+        # n_parallel = self.options["n_parallel"]
+        try:
             y_data[0][0][0] += 0
             self.ny = len(y_data)
-        except :
+        except:
             self.log("EGO will be used as there is only 1 objective")
             self.use_ego(fun, x_data, y_data)
-            self.log("Optimization done, get the front with .result.F and the set with .result.X")
+            self.log(
+                "Optimization done, get the front with .result.F and the set with .result.X"
+            )
             return
         if self.ny > 2 and self.options["criterion"] != "GA":
-            self.log("Only GA is available for more than 2 objectives at the moment, criterion will be switched")
+            self.log(
+                "Only GA is available for more than 2 objectives at the moment, criterion will be switched"
+            )
             self.options["criterion"] = "GA"
-            
-        #obtaining models for each objective
+
+        # obtaining models for each objective
         self.modelize(x_data, y_data)
-        self.probleme= self.def_prob()
-        
-        if type(y_data) != list :
+        self.probleme = self.def_prob()
+
+        if type(y_data) != list:
             y_data = list(y_data)
-        
+
         for k in range(self.options["n_iter"]):
-            
-            self.log(str("iteration "+str(k+1)))
+
+            self.log(str("iteration " + str(k + 1)))
 
             # find next best x-coord point to evaluate
-            new_x = self._find_best_point()                
+            new_x = self._find_best_point()
             new_y = fun(np.array([new_x]))
-            
-            #update model with the new point
-            for i in range(len(y_data)):  
-                y_data[i] = np.atleast_2d(np.append(y_data[i],new_y[i],axis=0))   
-            x_data = np.atleast_2d(np.append(x_data,np.array([new_x]),axis=0))
-            
+
+            # update model with the new point
+            for i in range(len(y_data)):
+                y_data[i] = np.atleast_2d(np.append(y_data[i], new_y[i], axis=0))
+            x_data = np.atleast_2d(np.append(x_data, np.array([new_x]), axis=0))
+
             self.modelize(x_data, y_data)
-            
+
         self.log("Model is well refined, NSGA2 is running...")
-        self.result = minimize(self.probleme,NSGA2(pop_size=self.options["pop_size"]),("n_gen",self.options["n_gen"]),verbose=False)
-        self.log("Optimization done, get the front with .result.F and the set with .result.X")
-    #return result.X , result.F
-        
-    def _setup_optimizer(self,fun):
+        self.result = minimize(
+            self.probleme,
+            NSGA2(pop_size=self.options["pop_size"]),
+            ("n_gen", self.options["n_gen"]),
+            verbose=False,
+        )
+        self.log(
+            "Optimization done, get the front with .result.F and the set with .result.X"
+        )
+
+    # return result.X , result.F
+
+    def _setup_optimizer(self, fun):
         """
         Parameters
         ----------
@@ -131,14 +171,17 @@ class MOO(SurrogateBasedApplication):
             yt[i] = f1(xt).
 
         """
-        if type(self.options["xdoe"]) == np.ndarray and type(self.options["ydoe"]) == np.ndarray:
+        if (
+            type(self.options["xdoe"]) == np.ndarray
+            and type(self.options["ydoe"]) == np.ndarray
+        ):
             return self.options["xdoe"], self.options["ydoe"]
         sampling = LHS(xlimits=self.options["xlimits"])
         xt = sampling(self.options["n_start"])
         yt = fun(xt)
         return xt, yt
-           
-    def modelize(self,xt,yt):
+
+    def modelize(self, xt, yt):
         """
         Creates and train a krige model with the given datapoints
 
@@ -148,11 +191,11 @@ class MOO(SurrogateBasedApplication):
             Design space coordinates of the training points.
         yt : TYPE
             Training outputs.
-        """        
+        """
         self.modeles = []
-        for iny in range(self.ny):   
-            t= KRG(print_global=False)
-            t.set_training_values(xt,yt[iny])        
+        for iny in range(self.ny):
+            t = KRG(print_global=False)
+            t.set_training_values(xt, yt[iny])
             t.train()
             self.modeles.append(t)
 
@@ -168,22 +211,24 @@ class MOO(SurrogateBasedApplication):
         n_var = self.ndim
         xbounds = self.options["xlimits"]
         modelizations = self.modeles
+
         class MyProblem(Problem):
-        
             def __init__(self):
-                super().__init__(n_var=n_var,
-                                 n_obj=n_obj,
-                                 n_constr=0,
-                                 xl=np.asarray([i[0] for i in xbounds]),
-                                 xu=np.asarray([i[1] for i in xbounds]),
-                                 elementwise_evaluation=True)
-        
+                super().__init__(
+                    n_var=n_var,
+                    n_obj=n_obj,
+                    n_constr=0,
+                    xl=np.asarray([i[0] for i in xbounds]),
+                    xu=np.asarray([i[1] for i in xbounds]),
+                    elementwise_evaluation=True,
+                )
+
             def _evaluate(self, x, out, *args, **kwargs):
-                xx = np.asarray(x).reshape(1, -1) #le modèle prend un array en entrée
+                xx = np.asarray(x).reshape(1, -1)  # le modèle prend un array en entrée
                 out["F"] = [i.predict_values(xx)[0][0] for i in modelizations]
-                        
-        return MyProblem()                
-        
+
+        return MyProblem()
+
     def _find_best_point(self):
         """
         Selects the best point to refine the model, according to the chosen method
@@ -194,68 +239,88 @@ class MOO(SurrogateBasedApplication):
             next point for the model update.
         """
         criter = self.options["criterion"]
-        
-        if criter == "GA" :
-            res = minimize(self.probleme,
-                   NSGA2(pop_size=self.options["pop_size"]),
-                   ("n_gen", self.options["n_gen"]),
-                   verbose=False)
-            X = res.X 
+
+        if criter == "GA":
+            res = minimize(
+                self.probleme,
+                NSGA2(pop_size=self.options["pop_size"]),
+                ("n_gen", self.options["n_gen"]),
+                verbose=False,
+            )
+            X = res.X
             Y = res.F
-            ydata = np.transpose(np.asarray([mod.training_points[None][0][1] for mod in self.modeles]))[0]
+            ydata = np.transpose(
+                np.asarray([mod.training_points[None][0][1] for mod in self.modeles])
+            )[0]
             xdata = self.modeles[0].training_points[None][0][0]
-            #MOBOpt criterion
+            # MOBOpt criterion
             q = self.options["q"]
             n = ydata.shape[1]
-            d_l_x = [sum([np.linalg.norm(xj-xi) for xj in xdata]) /n for xi in X]
-            d_l_f = [sum([np.linalg.norm(yj-yi) for yj in ydata]) /n for yi in Y]              
+            d_l_x = [sum([np.linalg.norm(xj - xi) for xj in xdata]) / n for xi in X]
+            d_l_f = [sum([np.linalg.norm(yj - yi) for yj in ydata]) / n for yi in Y]
             µ_x = np.mean(d_l_x)
             µ_f = np.mean(d_l_f)
             var_x, var_f = np.var(d_l_x), np.var(d_l_f)
-            if var_x ==0 or var_f == 0 :
-                return X[0,:]
-            dispersion = [ q*(d_l_x[j] - µ_x)/var_x + (1-q)*(d_l_f[j] - µ_f)/var_f for j in range(X.shape[0])]
+            if var_x == 0 or var_f == 0:
+                return X[0, :]
+            dispersion = [
+                q * (d_l_x[j] - µ_x) / var_x + (1 - q) * (d_l_f[j] - µ_f) / var_f
+                for j in range(X.shape[0])
+            ]
             i = dispersion.index(max(dispersion))
-            return X[i,:]#, Y[i,:]
-        
-        if criter == "PI" :
+            return X[i, :]  # , Y[i,:]
+
+        if criter == "PI":
             PI = criterion("PI", self.modeles)
             self.obj_k = lambda x: -PI(x)
-        if criter == "EHVI" :
-            ydata = np.transpose(np.asarray([mod.training_points[None][0][1] for mod in self.modeles]))[0]
-            ref = [ydata[:,0].max()+1, ydata[:,1].max()+1]
+        if criter == "EHVI":
+            ydata = np.transpose(
+                np.asarray([mod.training_points[None][0][1] for mod in self.modeles])
+            )[0]
+            ref = [ydata[:, 0].max() + 1, ydata[:, 1].max() + 1]
             EHVI = criterion("EHVI", self.modeles, ref)
             self.obj_k = lambda x: -EHVI(x)
-        if criter == "WB2S" :
-            ydata = np.transpose(np.asarray([mod.training_points[None][0][1] for mod in self.modeles]))[0]
-            ref = [ydata[:,0].max()+1, ydata[:,1].max()+1]
+        if criter == "WB2S":
+            ydata = np.transpose(
+                np.asarray([mod.training_points[None][0][1] for mod in self.modeles])
+            )[0]
+            ref = [ydata[:, 0].max() + 1, ydata[:, 1].max() + 1]
             EHVI = criterion("EHVI", self.modeles, ref)
             self.obj_k_inter = lambda x: -EHVI(x)
             xstart_inter = np.zeros(self.ndim)
             bounds = self.options["xlimits"]
             for i in range(self.ndim):
-                xstart_inter[i]= uniform(*bounds[i])
-            xmax = minimize1D(self.obj_k_inter, xstart_inter, bounds = bounds).x
+                xstart_inter[i] = uniform(*bounds[i])
+            xmax = minimize1D(self.obj_k_inter, xstart_inter, bounds=bounds).x
             EHVImax = EHVI(xmax)
             if EHVImax == 0:
                 s = 1
-            else :
+            else:
                 moyennes = [mod.predict_values for mod in self.modeles]
-                beta = 100 #to be discussed
-                s = beta * sum([abs(moy(np.asarray(xmax).reshape(1, -1))[0][0]) for moy in moyennes]) / EHVImax
-            WB2S = criterion("WB2S", self.modeles, ref,s)
+                beta = 100  # to be discussed
+                s = (
+                    beta
+                    * sum(
+                        [
+                            abs(moy(np.asarray(xmax).reshape(1, -1))[0][0])
+                            for moy in moyennes
+                        ]
+                    )
+                    / EHVImax
+                )
+            WB2S = criterion("WB2S", self.modeles, ref, s)
             self.obj_k = lambda x: -WB2S(x)
-            
+
         xstart = np.zeros(self.ndim)
         bounds = self.options["xlimits"]
         for i in range(self.ndim):
-            xstart[i]= uniform(*bounds[i])
-        return minimize1D(self.obj_k, xstart, bounds = bounds).x
-    
+            xstart[i] = uniform(*bounds[i])
+        return minimize1D(self.obj_k, xstart, bounds=bounds).x
+
     def log(self, msg):
         if self.options["verbose"]:
             print(msg)
-        
+
     def use_ego(self, fun, xdoe, ydoe):
         """
         Call ego to find the optimum of the 1D-valued funcion fun.
@@ -270,19 +335,22 @@ class MOO(SurrogateBasedApplication):
         """
         from smt.applications import EGO
         from pymoo.model.algorithm import Algorithm
+
         ego = EGO(
-        xdoe = xdoe,
-        ydoe = ydoe,
-        n_iter=self.options["n_iter"],
-        criterion="EI",
-        n_start=self.options["n_start"],
-        xlimits=self.options["xlimits"],
-        n_parallel = self.options["n_parallel"],
-        verbose = self.options["verbose"],
-        surrogate = self.options["surrogate"]
+            xdoe=xdoe,
+            ydoe=ydoe,
+            n_iter=self.options["n_iter"],
+            criterion="EI",
+            n_start=self.options["n_start"],
+            xlimits=self.options["xlimits"],
+            n_parallel=self.options["n_parallel"],
+            verbose=self.options["verbose"],
+            surrogate=self.options["surrogate"],
         )
         x_opt, y_opt, _, _, _ = ego.optimize(fun)
         self.result = Algorithm()
         self.result.X = np.array([[x_opt]])
         self.result.F = np.array([[y_opt]])
-        self.log("Optimization done, get the front with .result.F and the set with .result.X")
+        self.log(
+            "Optimization done, get the front with .result.F and the set with .result.X"
+        )
